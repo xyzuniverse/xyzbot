b3cc9f8df2fb8e2a3378dbcf0eacca08d60ae61e
xyzuniverse <dev.xyzuniverse@gmail.com>
2023-02-03 23:54:32 +0800

    xyzbot: Initial common script
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

---
+++ handler.js
@@ -0,0 +1,311 @@
+// Load the modules
+const helper = require("./lib/message_helper.js")
+
+// Exporting the modules
+module.exports = {
+  async handler(msgUpsert) {
+    let m = msgUpsert?.messages[0]
+    
+    // Prevent old messages synced
+    let msgTime = (new Date()/1000).toFixed(0) - m.messageTimestamp
+    if (msgTime > 1800) this.logger.info("Older messages has been detected, skipping.")
+    
+    try {
+      // Message helper
+      helper.messageHelper(this, m)
+ 
+      // Database manager
+      m.exp = 0
+      m.limit = false
+      try {
+        let user = global.db.data.users[m.sender]
+        if (typeof user !== 'object') global.db.data.users[m.sender] = {}
+        if (user) {
+          if (!isNumber(user.exp)) user.exp = 0
+          if (!isNumber(user.limit)) user.limit = 10
+          if (!isNumber(user.lastclaim)) user.lastclaim = 0
+          if (!('registered' in user)) user.registered = false
+          if (!user.registered) {
+            if (!('name' in user)) user.name = m.pushName
+            if (!isNumber(user.age)) user.age = -1
+            if (!isNumber(user.regTime)) user.regTime = -1
+          }
+          if (!isNumber(user.afk)) user.afk = -1
+          if (!('afkReason' in user)) user.afkReason = ''
+          if (!('banned' in user)) user.banned = false
+          if (!isNumber(user.pc)) user.pc = 0
+          if (!isNumber(user.level)) user.level = 0
+          if (!user.role) user.role = 'Beginner'
+          if (!('autolevelup' in user)) user.autolevelup = false
+        } else global.db.data.users[m.sender] = {
+          exp: 0,
+          limit: 10,
+          lastclaim: 0,
+          registered: false,
+          name: m.pushName,
+          age: -1,
+          regTime: -1,
+          afk: -1,
+          afkReason: '',
+          banned: false,
+          pc: 0,
+          level: 0,
+          role: 'Beginner',
+          autolevelup: false,
+        }
+
+        let chat = global.db.data.chats[m.chat]
+        if (typeof chat !== 'object') global.db.data.chats[m.chat] = {}
+        if (chat) {
+          if (!('isBanned' in chat)) chat.isBanned = false
+          if (!('welcome' in chat)) chat.welcome = false
+          if (!('detect' in chat)) chat.detect = false
+          if (!('sWelcome' in chat)) chat.sWelcome = ''
+          if (!('sBye' in chat)) chat.sBye = ''
+          if (!('sPromote' in chat)) chat.sPromote = ''
+          if (!('sDemote' in chat)) chat.sDemote = ''
+          if (!('delete' in chat)) chat.delete = true
+          if (!('antiLink' in chat)) chat.antiLink = false
+          if (!('antiSticker' in chat)) chat.antiSticker = false
+          if (!('getmsg' in chat)) chat.getmsg = false
+          if (!('simi' in chat)) chat.simi = false
+          if (!('viewonce' in chat)) chat.viewonce = false
+        } else global.db.data.chats[m.chat] = {
+          isBanned: false,
+          welcome: false,
+          detect: false,
+          sWelcome: '',
+          sBye: '',
+          sPromote: '',
+          sDemote: '',
+          delete: true,
+          antiLink: false,
+          antiSticker: false,
+          getmsg: false,
+          simi: false,
+          viewonce: false,
+        }
+
+        var setting = global.db.data.settings[this.user.jid]
+        if (typeof setting !== 'object') global.db.data.settings[this.user.jid] = {}
+        if (setting) {
+          if (!('anticall' in setting)) setting.anticall = false
+          if (!('autoread' in setting)) setting.autoread = false
+          if (!('nyimak' in setting)) setting.nyimak = false
+          if (!('restrict' in setting)) setting.restrict = false
+          if (!('self' in setting)) setting.self = false
+          if (!('pconly' in setting)) setting.pconly = false
+          if (!('gconly' in setting)) setting.gconly = false
+          if (!('jadibot' in setting)) setting.jadibot = false
+        } else global.db.data.settings[this.user.jid] = {
+          anticall: false,
+          autoread: false,
+          nyimak: false,
+          restrict: false,
+          self: false,
+          pconly: false,
+          gconly: false,
+          jadibot: false,
+        }
+      } catch (e) {
+        console.error(e)
+      }
+      
+      // Command handler
+      for (let name in global.plugins) {
+        let plugin = global.plugins[name]
+        if (!plugin) continue
+        if (plugin.disabled) continue
+        if (!setting.restrict) if (plugin.tags && plugin.tags.includes('admin')) continue
+        const str2Regex = str => str.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&')
+        let _prefix = plugin.customPrefix ? plugin.customPrefix : conn.prefix ? conn.prefix : global.prefix
+        let match = (_prefix instanceof RegExp ? // RegExp Mode?
+          [[_prefix.exec(m.text), _prefix]] :
+          Array.isArray(_prefix) ? // Array?
+            _prefix.map(p => {
+              let re = p instanceof RegExp ? // RegExp in Array?
+                p :
+                new RegExp(str2Regex(p))
+              return [re.exec(m.text), re]
+            }) :
+            typeof _prefix === 'string' ? // String?
+              [[new RegExp(str2Regex(_prefix)).exec(m.text), new RegExp(str2Regex(_prefix))]] :
+              [[[], new RegExp]]
+        ).find(p => p[1])
+        if (typeof plugin.before === 'function') if (await plugin.before.call(this, m, {
+          match,
+          conn: this,
+          participants,
+          groupMetadata,
+          user,
+          bot,
+          isROwner,
+          isOwner,
+          isAdmin,
+          isBotAdmin,
+          isPrems,
+          antiSticker,
+          chatUpdate,
+          isBlocked,
+        })) continue
+        if (typeof plugin !== 'function') continue
+        if ((usedPrefix = (match[0] || '')[0])) {
+          let noPrefix = m.text.replace(usedPrefix, '')
+          let [command, ...args] = noPrefix.trim().split` `.filter(v => v)
+          args = args || []
+          let _args = noPrefix.trim().split` `.slice(1)
+          let text = _args.join` `
+          command = (command || '').toLowerCase()
+          let fail = plugin.fail || global.dfail // When failed
+          let isAccept = plugin.command instanceof RegExp ? // RegExp Mode?
+            plugin.command.test(command) :
+            Array.isArray(plugin.command) ? // Array?
+              plugin.command.some(cmd => cmd instanceof RegExp ? // RegExp in Array?
+                cmd.test(command) :
+                cmd === command
+              ) :
+              typeof plugin.command === 'string' ? // String?
+                plugin.command === command :
+                false
+
+          if (!isAccept) continue
+          m.plugin = name
+          if (m.chat in global.db.data.chats || m.sender in global.db.data.users) {
+            let chat = global.db.data.chats[m.chat]
+            let user = global.db.data.users[m.sender]
+            if (name != 'unbanchat.js' && chat && chat.isBanned) return // Except this
+            if (name != 'unbanuser.js' && user && user.banned) return
+          }
+          if (plugin.rowner && plugin.owner && !(isROwner || isOwner)) { // Both Owner
+            fail('owner', m, this)
+            continue
+          }
+          if (plugin.rowner && !isROwner) { // Real Owner
+            fail('rowner', m, this)
+            continue
+          }
+          if (plugin.owner && !isOwner) { // Number Owner
+            fail('owner', m, this)
+            continue
+          }
+          if (plugin.mods && !isMods) { // Moderator
+            fail('mods', m, this)
+            continue
+          }
+          if (plugin.premium && !isPrems) { // Premium
+            fail('premium', m, this)
+            continue
+          }
+          if (plugin.group && !m.isGroup) { // Group Only
+            fail('group', m, this)
+            continue
+          } else if (plugin.botAdmin && !isBotAdmin) { // You Admin
+            fail('botAdmin', m, this)
+            continue
+          } else if (plugin.admin && !isAdmin) { // User Admin
+            fail('admin', m, this)
+            continue
+          }
+          if (plugin.private && m.isGroup) { // Private Chat Only
+            fail('private', m, this)
+            continue
+          }
+          if (plugin.register == true && _user.registered == false) { // Butuh daftar?
+            fail('unreg', m, this)
+            continue
+          }
+
+          m.isCommand = true
+          let xp = 'exp' in plugin ? parseInt(plugin.exp) : 17 // XP Earning per command
+          if (xp > 200) m.reply('Ngecit -_-') // Hehehe
+          else m.exp += xp
+          if (!isPrems && plugin.limit && global.db.data.users[m.sender].limit < plugin.limit * 1) {
+            this.send2Button(m.chat, `Limit anda habis, silahkan beli melalui *${usedPrefix}buy*`, '© wabot-aq', 'Buy', `${usedPrefix}buy`, 'Buy All', `${usedPrefix}buyall`, m)
+            continue // Limit habis
+          }
+          if (plugin.level > _user.level) {
+            this.reply(m.chat, `Diperlukan level *${plugin.level}* untuk menggunakan perintah ini. Level kamu ${_user.level}`, m)
+            continue // If the level has not been reached
+          }
+          let extra = {
+            match,
+            usedPrefix,
+            noPrefix,
+            _args,
+            args,
+            command,
+            text,
+            conn: this,
+            participants,
+            groupMetadata,
+            user,
+            bot,
+            isROwner,
+            isOwner,
+            isAdmin,
+            isBotAdmin,
+            isPrems,
+            chatUpdate,
+            isBlocked,
+          }
+          try {
+            await plugin.call(this, m, extra)
+            if (!isPrems) m.limit = m.limit || plugin.limit || false
+          } catch (e) {
+            // Error occured
+            m.error = e
+            console.error(e)
+            if (e) {
+              let text = util.format(e.message ? e.message : e)
+              for (let key of Object.values(global.APIKeys))
+                text = text.replace(new RegExp(key, 'g'), '#HIDDEN#')
+              m.reply(text)
+            }
+          } finally {
+            // m.reply(util.format(_user))
+            if (typeof plugin.after === 'function') {
+              try {
+                await plugin.after.call(this, m, extra)
+              } catch (e) {
+                console.error(e)
+              }
+            }
+          }
+          break
+        }
+      }
+    } finally {
+      console.log(JSON.stringify(m, null, 2))
+      let user, stats = global.db.data.stats
+      if (m) {
+        if (m.sender && (user = global.db.data.users[m.sender])) {
+          user.exp += m.exp
+          user.limit -= m.limit * 1
+        }
+
+        let stat
+        if (m.plugin) {
+          let now = + new Date
+          if (m.plugin in stats) {
+            stat = stats[m.plugin]
+            if (!isNumber(stat.total)) stat.total = 1
+            if (!isNumber(stat.success)) stat.success = m.error != null ? 0 : 1
+            if (!isNumber(stat.last)) stat.last = now
+            if (!isNumber(stat.lastSuccess)) stat.lastSuccess = m.error != null ? 0 : now
+          } else stat = stats[m.plugin] = {
+            total: 1,
+            success: m.error != null ? 0 : 1,
+            last: now,
+            lastSuccess: m.error != null ? 0 : now
+          }
+          stat.total += 1
+          stat.last = now
+          if (m.error == null) {
+            stat.success += 1
+            stat.lastSuccess = now
+          }
+        }
+      }
+    }
+  }
+}
---
+++ index.js
@@ -0,0 +1,154 @@
+// Load the modules
+const {
+    default: initWASocket,
+    DisconnectReason,
+    fetchLatestBaileysVersion,
+    useMultiFileAuthState,
+} = require('@adiwajshing/baileys')
+
+const logger = require("pino")({
+    transport: {
+        target: "pino-pretty",
+        options: {
+            levelFirst: true,
+            ignore: "hostname",
+            translateTime: true
+        }
+    }
+}).child({ class: "Baileys" })
+const { Boom } = require('@hapi/boom')
+const fs = require('fs')
+const store_system = require("./lib/store.js")
+const { Low, JSONFile } = require('./lib/lowdb')
+const path = require("path")
+const _ = require('lodash')
+const yargs = require("yargs/yargs")
+
+// Prevent to crash if error occured
+process.on('uncaughtException', console.error)
+
+// Database
+global.db = new Low(
+    new JSONFile(`./database.json`)
+)
+
+global.loadDatabase = async function loadDatabase() {
+  if (global.db.READ) return new Promise((resolve) => setInterval(function () { (!global.db.READ ? (clearInterval(this), resolve(global.db.data == null ? global.loadDatabase() : global.db.data)) : null) }, 1 * 1000))
+  if (global.db.data !== null) return
+  global.db.READ = true
+  await global.db.read()
+  global.db.READ = false
+  global.db.data = {
+    users: {},
+    chats: {},
+    stats: {},
+    msgs: {},
+    sticker: {},
+    settings: {},
+    list: {},
+    ...(global.db.data || {})
+  }
+  global.db.chain = _.chain(global.db.data)
+}
+global.loadDatabase()
+
+if (global.db) setInterval(async () => {
+    if (global.db.data) await global.db.write()
+    if (!opts['tmp'] && (global.support || {}).find) (tmp = [os.tmpdir(), 'tmp'], tmp.forEach(filename => cp.spawn('find', [filename, '-amin', '3', '-type', 'f', '-delete'])))
+    process.on("exit", async () => await global.db.write())
+  }, 30 * 1000)
+
+// Plugin manager
+let pluginFolder = path.join(__dirname, 'plugins')
+let pluginFilter = filename => /\.js$/.test(filename)
+global.plugins = {}
+for (let filename of fs.readdirSync(pluginFolder).filter(pluginFilter)) {
+  try {
+    global.plugins[filename] = require(path.join(pluginFolder, filename))
+  } catch (e) {
+    logger.error(e)
+    delete global.plugins[filename]
+  }
+}
+console.log(Object.keys(global.plugins))
+global.reload = (_ev, filename) => {
+  if (pluginFilter(filename)) {
+    let dir = path.join(pluginFolder, filename)
+    if (dir in require.cache) {
+      delete require.cache[dir]
+      if (fs.existsSync(dir)) logger.info(`re - require plugin '${filename}'`)
+      else {
+        logger.warn(`deleted plugin '${filename}'`)
+        return delete global.plugins[filename]
+      }
+    } else logger.info(`requiring new plugin '${filename}'`)
+    let err = syntaxerror(fs.readFileSync(dir), filename)
+    if (err) logger.error(`syntax error while loading '${filename}'\n${err}`)
+    else try {
+      global.plugins[filename] = require(dir)
+    } catch (e) {
+      logger.error(e)
+    } finally {
+      global.plugins = Object.fromEntries(Object.entries(global.plugins).sort(([a], [b]) => a.localeCompare(b)))
+    }
+  }
+}
+Object.freeze(global.reload)
+fs.watch(path.join(__dirname, 'plugins'), global.reload)
+
+// Options
+global.opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
+
+// Prefix
+global.prefix = new RegExp('^[' + (opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
+
+/**
+ * Init the WA Web connection
+ * @returns
+ */
+const initConnection = async () => {
+    const { state, saveCreds } = await store_system.useMultiFileAuthState("sessions")
+    const { version, isLatest } = await fetchLatestBaileysVersion()
+    logger.info(`connecting using version ${version.join(`.`)}, latest: ${isLatest}`)
+
+    // Initialize the events
+    global.conn = initWASocket({
+        printQRInTerminal: true,
+        auth: state,
+        logger: logger,
+        version
+    })
+    
+    // Load connection helper
+    require("./lib/message_helper").connectionHelper(conn)
+    
+    // Add logger into socket
+    conn.logger = logger
+
+    // Connection update event
+    conn.ev.on("connection.update", async update => {
+        var { connection, lastDisconnect } = update
+        if (connection === 'close') {
+            logger.info("conection lost, reconnecting for a few seconds...")
+            let reason = new Boom(lastDisconnect?.error)?.output?.statusCode;
+            if (reason !== DisconnectReason.loggedOut) {
+                initConnection()
+            } else { 
+                logger.error("Disconnected from server because you're logged out, please regenerate a new session.")
+                conn.logout()
+                fs.unlinkSync("./sessions")
+                process.exit()
+            }
+        }
+        if (global.db.data === null) await global.loadDatabase()
+    })
+
+    // Credentials update event
+    conn.ev.on("creds.update", saveCreds)
+
+    // Chats message event
+    conn.ev.on("messages.upsert", require("./handler.js").handler.bind(conn))
+    return conn
+}
+
+initConnection().catch(e => console.log(e))
