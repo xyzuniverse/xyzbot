cb15423d2d1b0a2d7882d9e890ad71166bf65387
xyzuniverse <dev.xyzuniverse@gmail.com>
2023-04-02 00:53:33 +0800

    xyzbot: messageHelper: Fixup message type * Sometimes we get a senderKeyDistributionMessage,   so let's add a getContentType from baileys one.
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- socketHelper.js
+++ socketHelper.js
@@ -1,3 +1,4 @@
+const { getContentType } = require('baileys')
 exports.messageHelper = (conn, m) => {
     if (!m) return m
     let id = m.key?.id
@@ -6,10 +7,10 @@ exports.messageHelper = (conn, m) => {
     let name = m.pushName
     let fromMe = m.key?.fromMe
     let message = m.message
-    let type = Object.keys(m.message)[0]
+    let type = getContentType(m.message) || Object.keys(m.message)[0]
     let messageContent = m.message[type]
     if (type === 'ephemeralMessage') {
-        type = Object.keys(m.message[type].message)[0]
+        type = getContentType(m.message[type].message) || Object.keys(m.message[type].message)[0]
         messageContent = m.message.ephemeralMessage.message[type]
     }
     let text = messageContent.caption ? messageContent.caption : messageContent.text ? messageContent.text : typeof messageContent === "string" ? messageContent : ""
@@ -18,15 +19,17 @@ exports.messageHelper = (conn, m) => {
     if (messageContent && typeof messageContent === 'object') {
        if (messageContent?.contextInfo?.mentionedJid?.length && messageContent.contextInfo.mentionedJid) mentionedJid = messageContent.contextInfo.mentionedJid
        if (messageContent?.contextInfo?.quotedMessage) {
-           let qtype = Object.keys(messageContent.contextInfo.quotedMessage)[0]
+           let qtype = getContentType(messageContent.contextInfo.quotedMessage) || Object.keys(messageContent.contextInfo.quotedMessage)[0]
            quoted = messageContent.contextInfo.quotedMessage[qtype]
            if (typeof quoted === 'string') quoted = { text: quoted }
+           q_type = qtype
            q_id = messageContent.contextInfo?.stanzaId
            q_chat = chat
            q_sender = messageContent.contextInfo?.participant
            q_text = quoted.text || quoted.caption || quoted.contentText || ''
            q_mentionedJid = quoted.contextInfo?.mentionedJid?.length && quoted.contextInfo.mentionedJid || []
            quoted = {
+            type: q_type,
             id: q_id,
             chat: q_chat,
             sender: q_sender,
