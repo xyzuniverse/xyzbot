f148fe07dcac9900f8524673e6d8e49eb2ac5832
xyzuniverse <dev.xyzuniverse@gmail.com>
2023-05-04 19:30:48 +0800

    xyzbot: Add group handling
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- handler.js
+++ handler.js
@@ -1,6 +1,21 @@
 module.exports = {
     async handler(client, messages) {
         if (!messages) return;
+
+        // Filter users
+        let chats = await messages.getChat();
+        let users = await messages.getContact();
+
+        let isGroup = messages.from.endsWith("@g.us");
+        let groupMetadata = isGroup ? chats.groupMetadata : {};
+        let participants = isGroup ? groupMetadata.participants : [];
+
+        let user = isGroup ? participants.find(u => u.id.user == users.number) : {};
+        let bot = isGroup ? participants.find(u => u.id.user == client.info.me.user) : {};
+
+        let isAdmin = isGroup ? (user.isAdmin || user.isSuperAdmin) : false;
+        let isBotAdmin = isGroup ? (bot.isAdmin || bot.isSuperAdmin) : false;
+
         if (messages.body.startsWith("!eval")) {
             try {
                 evaled = await eval(messages.body.slice(6))
