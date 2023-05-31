9ed83b876a9b2596e72cb3a94d4fe5b3fc921b69
xyzuniverse <dev.xyzuniverse@gmail.com>
2023-06-01 04:50:46 +0800

    xyzbot: Add hoyolab-api feature
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

---
+++ plugins/hoyoverse/redeemcode.js
@@ -0,0 +1,34 @@
+const { Genshin, HonkaiStarRail, GamesEnum, Request, Route } = require("@vermaysha/hoyolab-api");
+
+let handler = async (msg, { command, text }) => {
+  if (!text) return msg.reply("Masukkan kode redeem!");
+  let user = global.db.data.users[msg.author];
+  if (!user?.hoyolab && !user?.hoyolab.cookieToken)
+    return msg.reply("Cookie token tidak ditemukan! Silahan generate cookie token di hoyolab website.\nTutorial coming soon.");
+  var result;
+  try {
+    let _game = command.split("redeemcode")[0] == "gi" ? GamesEnum.GENSHIN_IMPACT : GamesEnum.HONKAI_STAR_RAIL;
+    let options = { cookie: user.hoyolab.cookieToken, lang: "id", uid: user.hoyolab[_game].uid, region: user.hoyolab[_game].server };
+    let game = command.split("redeemcode")[0] == "gi" ? new Genshin(options) : new HonkaiStarRail(options);
+    if (command.includes("hsr")) {
+      game.request.setParams({
+        uid: user.hoyolab[_game].uid,
+        region: user.hoyolab[_game].server,
+        game_biz: "hkrpg_global",
+        cdkey: text.replace(/\uFFFD/g, ""),
+        lang: "id",
+        sLangKey: "id",
+      });
+      result = await game.request.send(Route.GENSHIN_REDEEM_CODE.replace("hk4e", "hkrpg").replace("hoyolab", "hoyoverse"));
+    } else result = await game.redeemCode(text);
+  } catch (e) {
+    console.error(e);
+    msg.reply(e);
+    msg.reply("A error occured, please try again later.");
+  } finally {
+    msg.reply("```" + JSON.stringify(result, null, 2) + "```");
+  }
+};
+
+handler.command = ["hsrredeemcode", "giredeemcode"];
+module.exports = handler;
