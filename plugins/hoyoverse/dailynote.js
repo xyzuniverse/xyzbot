33611d6fabae440cd89494f79487707a7030c587
xyzuniverse <dev.xyzuniverse@gmail.com>
2023-06-07 18:38:37 +0800

    xyzbot: hoyolab: Add Daily realtime & Game record history
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

---
+++ plugins/hoyoverse/dailynote.js
@@ -0,0 +1,66 @@
+const { Genshin, HonkaiStarRail: _HSR, GamesEnum } = require("@vermaysha/hoyolab-api");
+const { HonkaiStarRail } = require("../../lib/hoyolab-api.getuserinfo");
+const StarRail = HonkaiStarRail(_HSR);
+
+let handler = async (msg, { command }) => {
+  let user = global.db.data.users[msg.author];
+  if (!user?.hoyolab?.cookieToken)
+    return msg.reply("Cookie token tidak ditemukan! Silahan generate cookie token di hoyolab website.\nTutorial coming soon.");
+  var result;
+  try {
+    let _game = command.split("dailynote")[0] == "gi" ? GamesEnum.GENSHIN_IMPACT : GamesEnum.HONKAI_STAR_RAIL;
+    let options = { cookie: user.hoyolab.cookieToken, lang: "id-id", uid: user.hoyolab[_game].uid };
+    let game = command.split("dailynote")[0] == "gi" ? new Genshin(options) : new StarRail(options);
+    result = await game.dailyNote();
+  } catch (e) {
+    console.error(e);
+    msg.reply(e);
+    msg.reply("A error occured, please try again later.");
+  } finally {
+    if (command.startsWith("hsr")) {
+      let _expeditions = [];
+      let { current_stamina, max_stamina, stamina_recover_time, accepted_epedition_num, total_expedition_num, expeditions } = result;
+      expeditions.forEach((avatar) => {
+        _expeditions.push(`- ${avatar.name}, Status: ${avatar.status === "Finished" ? "Selesai, belum diclaim" : "Masih berjalan"}`);
+      });
+      msg.reply(
+        "```" +
+          `Current Trailblazer Power: ${current_stamina}/${max_stamina}\nPelaksanaan Tugas (${accepted_epedition_num}/${total_expedition_num})\nExpeditions/Tugas:\n${_expeditions.join(
+            "\n"
+          )}` +
+          "```"
+      );
+    } else {
+      let _giexpeditions = [];
+      let {
+        current_resin,
+        max_resin,
+        resin_recovery_time,
+        finished_task_num,
+        total_task_num,
+        expeditions,
+        current_expedition_num,
+        max_expedition_num,
+      } = result;
+      expeditions.forEach((avatar) => {
+        _giexpeditions.push(
+          `- ${avatar.avatar_side_icon.split("Side_")[1].split(".png")[0]}, Status: ${
+            avatar.status === "Finished" ? "Selesai, belum diclaim" : "Masih berjalan"
+          }`
+        );
+      });
+      msg.reply(
+        "```" +
+          `Current Resin: ${current_resin}/${max_resin}\nEkspedisi status (${current_expedition_num}/${max_expedition_num})\n${finished_task_num}/${total_task_num} Misi harian diselesaikan\nEkspedisi/Tugas:\n${_giexpeditions.join(
+            "\n"
+          )}` +
+          "```"
+      );
+    }
+  }
+};
+
+handler.help = ["hsrdailynote", "gidailynote"];
+handler.tags = ["hyv"];
+handler.command = ["hsrdailynote", "gidailynote"];
+module.exports = handler;
