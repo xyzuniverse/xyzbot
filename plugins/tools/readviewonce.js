007d801dbc9f8ea58bb48cc49f94802c8c1ad105
xyzuniverse <dev.xyzuniverse@gmail.com>
2023-07-04 13:46:39 +0800

    xyzbot: Improve readviewonce command
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- plugins/tools/readviewonce.js
+++ plugins/tools/readviewonce.js
@@ -7,12 +7,12 @@ let handler = async (msg, { client, text }) => {
     if (buffer) {
       msg.react("✅");
       if (text == "private") {
-        client.sendMessage(msg.author, { [q.type.replace(/message$/i, "")]: Buffer.from(buffer) }, { quoted: msg });
-      } else msg.reply({ [q.type.replace(/message$/i, "")]: Buffer.from(buffer) });
+        client.sendMessage(msg.author, { [q.type.replace(/message$/i, "")]: Buffer.from(buffer), caption: q.body ? q.body : "" }, { quoted: msg });
+      } else msg.reply({ [q.type.replace(/message$/i, "")]: Buffer.from(buffer), caption: q.body ? q.body : "" });
     } else return msg.reply("Getting media failed.");
   } else return msg.reply("Reply/include a view-once message then execute this command.");
 };
 
-handler.command = /^(readviewonce)$/i;
+handler.command = /^(readviewonce|rvo)$/i;
 
 module.exports = handler;
