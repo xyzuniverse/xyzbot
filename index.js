7b5ebd47d1472da7748f453c9815447d8d6c1aaa
xyzuniverse <dev.xyzuniverse@gmail.com>
2024-12-20 03:19:38 +0800

    main: Add serializer for message handling
    Signed-off-by: xyzuniverse <dev.xyzuniverse@gmail.com>

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
 const client = require("./lib/client");
+const Serializer = require("./lib/Serializer");
+
+// Prevent exit if it's closed
+process.on("uncaughtException", console.error);
 
 async function start() {
   const sessionDir = "sessions"; // Change anything if u want
   const bot = await client.connect(sessionDir);
 
   // Message event
-  bot.ev.on("messages.upsert", (m) => {
-    console.log(JSON.stringify(m, null, 2));
+  bot.ev.on("messages.upsert", (messages) => {
+    const msg = Serializer.serializeMessage(bot, messages.messages[0]);
+    console.log(JSON.stringify(msg, null, 2));
+    if (msg.text.toLowerCase() === "test") {
+      msg.react("🏓");
+      msg.reply("Pong!");
+    }
   });
 
   return bot;
