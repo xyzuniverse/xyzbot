9314ad7b4bcafc4c31b2ad3ceef2d19f4612c123
daylight021 <161336412+daylight021@users.noreply.github.com>
2025-07-11 20:28:55 +0700

    Update index.js

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ async function start() {
   const { version } = await fetchLatestBaileysVersion();
 
   // Gunakan Store Kustom kita di sini
-  const store = createCustomStore({ logger: Pino({ level: "info" }) });
+  const store = createCustomStore({ logger: Pino({ level: "silent" }) });
 
   const bot = makeWASocket({
     version,
